d15e8785ca8f2754e5eeef61cae4d87d3464ede5
Cheulong Sear <searcheulong@gmail.com>
2025-07-22 19:21:23 +0700

    add: terraform remote state

---
+++ terraform/aws/terraform-remote-state/3.backend.tf
@@ -0,0 +1,9 @@
+terraform {
+#   backend "s3" {
+#     bucket = "sfjljw34-terraform-remote-state"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     use_lockfile = true
+#     encrypt = true
+#     }
+}
